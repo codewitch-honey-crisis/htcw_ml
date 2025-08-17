ab69b75afa77831bec6ae9663b58d8e3058230c9
Andy Walker <andy@andy.dev>
2025-08-17 12:07:17 -0400

    fix: CDATA content includes `CDATA`

--- src/ml_reader.hpp
+++ src/ml_reader.hpp
@@ -471,6 +471,7 @@ namespace ml {
                                 m_state = (int)ml_node_type::error_eof;
                                 return 1;
                             }
+                            m_source.clear_capture();
                             m_state = state_cdata;
                             return parse_cdata_part();
                         } else if('-'==m_source.current()) {
